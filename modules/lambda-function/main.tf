4a97f9383a806d522d8d76d31da4c253817f750d
Alejandro Anzola <anzola95avila@gmail.com>
2021-10-27 20:20:22 -0500

    changes something
    Signed-off-by: Alejandro Anzola <anzola95avila@gmail.com>

--- modules/lambda-function/main.tf
+++ modules/lambda-function/main.tf
@@ -126,7 +126,7 @@ data "aws_iam_policy_document" "lambda_exec_role_policy_without_createloggroup" 
       "logs:PutLogEvents"
     ]
     resources = [
-      "arn:aws:logs:*:*:*"
+      aws_cloudwatch_log_group.lambda.arn
     ]
   }
 }
